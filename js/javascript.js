1b24c1b6cac542c8120bbe2e91d4611223652157
Eliška Siváková <elis7279@stud.kea.dk>
2019-01-08 22:35:17 +0100

    final

---
+++ js/javascript.js
@@ -0,0 +1,144 @@
+// Open and Close Burger menu //
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+// Basic Web //
+function bweb() {
+    window.location ="basicweb.html";
+}
+
+// Basic Content //
+    function bcon() {
+    window.location ="basiccontent.html";
+}
+// Basic Animation //
+    function bani() {
+    window.location ="basicanimation.html";
+}
+
+// Basic UX //
+    function bux() {
+    window.location ="basicux.html";
+}
+
+
+///// B U T T O N S /////
+
+// ABOUT ME //
+function btnCV() {
+    window.open ("http://eliskasivakova.com/KEA/CV.pdf");
+}
+
+// BASIC WEB //
+/* Responsive Web */
+function btn1() {
+    window.open ("http://eliskasivakova.com/KEA/BASIC-WEB/Responsive-web/index01.html");
+}
+
+/* Process document */
+function btn() {
+    window.open ("http://eliskasivakova.com/KEA/BASIC-WEB/Design_and_process_document.pdf");
+}
+
+/* BASIC CONTENT */
+/* STORY WITHOUT WORDS */
+function btn2() {
+    window.open("https://www.youtube.com/watch?v=lA_28B8WMdM");
+}
+
+
+// MICROSITE //
+/* XD prototype */
+function btn3() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-CONTENT/Microsite/Microsite-XD-Prototype.pdf");
+}
+
+/* Treatment */
+function btn4() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-CONTENT/Microsite/Treatment.pdf");
+}
+
+/* Storyboard */
+function btn5() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-CONTENT/Microsite/Storyboard.pdf");
+}
+
+/* Documentation */
+function btn6() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-CONTENT/Microsite/Documentation.pdf");
+}
+
+/* Final Microsite */
+function btn7() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-CONTENT/Microsite/index.html");
+}
+
+
+// BASIC ANIMATION //
+/* Simple animation */
+function btn8() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-ANIMATION/Simple-animation/animation.html");
+}
+
+/* Storyteling */
+function btn9() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-ANIMATION/Storyteling/loffy.html");
+}
+
+/* Storyteling pdf doc */
+function btn10() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-ANIMATION/Storyteling/03.02.01_Eliska_Sivakova.pdf");
+}
+
+/* Interactive animation */
+function btn11() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-ANIMATION/Interactive-animation/index.html");
+}
+
+/* Interactive animation - Sequence diagram */
+function btn111() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-ANIMATION/Interactive-animation/sequence-diagram.pdf");
+}
+
+/* Project Pool */
+function btn12() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-ANIMATION/Project-Pool/projectpool.html");
+}
+
+/* Interactive production */
+function btn13() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-ANIMATION/Interactive-production/pinocchio.html");
+}
+
+/* Screencast */
+function btn14() {
+    window.open("https://www.youtube.com/watch?v=hvJaij3YrTg");
+}
+
+
+// BASIC UX //
+/* Web prototype */
+function btn15() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-UX/Web-prototype/login.html");
+}
+
+/* XD prototype */
+function btn16() {
+    window.open("https://xd.adobe.com/view/4126d7dc-e163-47f9-4545-e73fbdb79a77-3717/");
+}
+
+/* XD prototype Native App */
+function btn17() {
+    window.open("https://xd.adobe.com/view/db99b92a-b032-4e10-6184-1f05c396f568-ada8/");
+}
+
+/* Theme documentation */
+function btn18() {
+    window.open("http://eliskasivakova.com/KEA/BASIC-UX/04-04-02_theme-documentation_Eliska-Sivakova.pdf");
+}
